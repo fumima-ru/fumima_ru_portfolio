1c515ce0e6c07c5d3e86444d5d1d9fb417fa1d89
fumima-ru <marumefumiko@gmail.com>
2022-12-30 15:15:52 +0900

    ポートフォリオサイトをgithubで共有

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* mainvisualのwelcomeを初期段階では消して少ししてからフワッと出現させる */
+/* img使わなかった分後で消去する */
+$(function() {
+ /*$('#contact a').hover(function() {
+    $('#contact a .img1').hide();
+    $('#contact a .img2').show();
+  });*/
+});
